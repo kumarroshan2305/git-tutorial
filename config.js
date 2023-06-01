c5558f32bb7fb9ad64e4ecdc177beed7401e804b
Roshan Kumar <kumar.roshan2305@gmail.com>
2023-06-01 13:47:19 +0530

    clone repoistery:

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ console.log('config.js');
 console.log('version51');
 console.log('changed 4th times');
 console.log('updation after github installed');
+console.log('clone repositery updated');
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ console.log('index.js');
 console.log('version123');
 console.log('change 4th time');
 console.log('github repostiery');
+console.log('git clone repositery');
